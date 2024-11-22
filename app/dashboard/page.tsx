4357cf68bd80aef59161c80200edf32c1deda05e
iBz-04 <issakaibrahimrayamah@gmail.com>
2024-11-23 02:43:04 +0300

    dashboard sidebar additions

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -13,7 +13,7 @@ export default async function Dashboard() {
       <Card className='w-[20rem]'>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">
-            Welcome to Nextjs Starter Kit
+            Welcome to OmniBot
           </CardTitle>
           <svg
             xmlns="http://www.w3.org/2000/svg"
